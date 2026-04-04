8d1e06bc446736bf7d2a634442750ebc93422e3f
obicharis <charisobinna4@gmail.com>
2026-04-05 00:42:29 +0100

    Remove node-fetch import from server.js
    Removed the 'node-fetch' dependency from server.js.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const fetch = require("node-fetch");
 const path = require("path");
 
 const app = express();
